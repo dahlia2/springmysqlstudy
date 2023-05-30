6575df26f474bb0c0e75c1e8dff690a80ed293ea
dahlia2 <dahlia2p@gmail.com>
2023-05-30 22:43:12 +0900

    2023-05-30

---
+++ 12_Pjt_User/src/main/java/com/gdu/app12/batch/SleepUserHandleScheduler.java
@@ -0,0 +1,25 @@
+package com.gdu.app12.batch;
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.gdu.app12.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+@EnableScheduling
+public class SleepUserHandleScheduler {
+
+  // field
+  private final UserService userService;
+  
+  // 매일 새벽 1시
+  @Scheduled(cron="0 0/1 * 1/1 * ?")
+  public void execute() {
+    userService.sleepUserHandle();
+  }
+  
+}
